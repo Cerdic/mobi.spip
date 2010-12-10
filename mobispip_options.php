1ff54df62b3ec94de8508dc06df6423cf21bfeca
Cerdic <cedric@yterium.com>
2010-12-10 23:08:02 +0000

    voila deja tout jquerymobile pose et une page d'acceuil fonctionnelle (inspiree de ispip)
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_/mobi.spip@42805 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ mobispip_options.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin mobiSPIP
+ * 
+ * Distribue sous licence GPL 
+ * (c) 2010 cedric
+ * Date: 10/12/10 - 22:10
+ *
+ */
+
+define('_VERSION_MOBILE',true);
+
+if (defined('_VERSION_MOBILE') AND _VERSION_MOBILE) {
+	_chemin(_DIR_PLUGIN_MOBISPIP.'mobile');
+  $GLOBALS['marqueur'].="mobispip:";
+	$GLOBALS['z_blocs'] = array('content','header','footer','head','head_js');
+}
