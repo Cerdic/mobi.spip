5c0cf0a70d87c16c7b95bc7201f5ef6da112c21b
Cerdic <cedric@yterium.com>
2010-12-18 11:19:21 +0000

    finalisation : page recherche, bouton de retour a l'accueil en haut a droite sur toutes les pages, champ de recherche en pied de page, chaines de langues
    git-svn-id: svn://zone.spip.org/spip-zone/_squelettes_/mobi.spip@42989 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/mobispip_fr.php
@@ -0,0 +1,15 @@
+<?php
+
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+'accueil' => 'accueil',
+'infos'=> 'Infos',
+'versionclassique' => 'Version classique du site',
+'mobispip' => 'mobi.SPIP',
+'annuler' => 'Annuler',
+'a_la_une' => '&Agrave; la une',
+'retour' => 'retour'
+);
+
+?>
