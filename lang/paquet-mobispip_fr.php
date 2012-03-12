77512242ebf05c76ef3484ef295185a51c5af932
jfefe <jfefe@aternatik.fr>
2012-03-12 01:48:55 +0100

    Fichier lang pour le plugin

---
+++ lang/paquet-mobispip_fr.php
@@ -0,0 +1,13 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mobispip_description' => 'Squelettes Z mobile pour SPIP (jQuery mobile)',
+	'mobispip_slogan' => 'Squelettes Z mobile pour SPIP (jQuery mobile)',
+);
+?>
