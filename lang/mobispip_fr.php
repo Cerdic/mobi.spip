0e72e89d25c993aa1df3a69ddd36f271f743f00b
jfefe <jfefe@aternatik.fr>
2012-03-12 01:49:18 +0100

    Fichier de langue squelettes

--- lang/mobispip_fr.php
+++ lang/mobispip_fr.php
@@ -3,14 +3,39 @@
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
-'accueil' => 'accueil',
-'infos'=> 'Infos',
-'version_classique' => 'Version classique du site',
-'version_mobile' => 'Version mobile du site',
-'mobispip' => 'mobi.SPIP',
-'annuler' => 'Annuler',
-'a_la_une' => '&Agrave; la une',
-'retour' => 'retour'
+
+	// A
+	'a_la_une' => '&Agrave; la une',
+	'accueil' => 'accueil',
+	'annuler' => 'Annuler',
+	'auteurs' => 'Les auteurs',
+	
+	// B
+	'breves' => 'Brèves',
+
+	// C
+	'config_theme' => 'Configuration du thème',
+	'config_theme_page' => 'Thème des pages',
+	'config_theme_liste' => 'Thème des listes',
+	'config_theme_divider' => 'Thème des divisions de listes',
+	
+	// I
+	'infos'=> 'Infos',
+	
+	// M
+	'meme_rubrique' => 'Dans la même rubrique',
+	'mobispip' => 'mobi.SPIP',
+	
+	// R
+	'retour' => 'retour',
+	
+	// S
+	'site' => 'A propos du site',
+	'sommaire' => 'Sommaire',
+	
+	// V
+	'version_classique' => 'Version classique du site',
+	'version_mobile' => 'Version mobile du site'
 );
 
 ?>
